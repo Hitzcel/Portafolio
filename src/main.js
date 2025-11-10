e3f8125607cdd6b0bd978650be42ba2a64c6b0c1
Hitzcel Crúz <hitzcel233@gmail.com>
2025-11-09 23:55:21 -0600

    Actualiza proyecto

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import './style.less';
+import projects from './data/projects.json';
+import ArtesanCafe from './assets/ArtesanCafe.png';
+import NoveliaEditorial from './assets/NoveliaEditorial.png';
+
+const imagesMap = {
+    "ArtesanCafe.png": ArtesanCafe,
+    "NoveliaEditorial.png": NoveliaEditorial
+};
+
+const container = document.getElementById("proyectos");
+
+projects.forEach(p => {
+    container.innerHTML += `
+  <a class="project" href="#">
+    <div class="project__image">
+      <img src="${imagesMap[p.image]}" alt="${p.title}">
+    </div>
+    <div class="project__title">${p.title}</div>
+  </a>
+  `;
+});
+
